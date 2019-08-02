61d4b4c99a052f981b0da97f74ea28c320412872
vizhali <52244020+vizhali@users.noreply.github.com>
2019-08-02 14:52:08 +0530

    Update pro16.py

--- pro16.py
+++ pro16.py
@@ -1,11 +1,11 @@
 vizh=int(input())
-ammu=list(map(int,input().split()))
+pop=list(map(int,input().split()))
 y=[1]*vizh
 for i in range(vizh):
     if i==0:
-        if ammu[i]>cat[i+1]:
+        if pop[i]>pop[i+1]:
             y[i]=y[i]+y[i+1]
     elif i>0:
-        if ammu[i]>ammu[i-1]:
+        if pop[i]>pop[i-1]:
             y[i]=y[i]+y[i-1]
 print(sum(y))
