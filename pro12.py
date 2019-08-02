9b1db373ac959adbe2eeb9d960134eedf29a4a46
vizhali <52244020+vizhali@users.noreply.github.com>
2019-08-02 13:47:24 +0530

    Update pro12.py

--- pro12.py
+++ pro12.py
@@ -1,5 +1,5 @@
-vizh,pop = map(int, input().split())
+vizha,popu = map(int, input().split())
 lst = list(map(int, input().split()))
-for i in range(pop):
+for i in range(popu):
   shiv,vani = map(int, input().split())
   print(min(lst[shiv-1:vani]))
