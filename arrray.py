279535816699957ea0ce9381af8cb450927196bc
vizhali <52244020+vizhali@users.noreply.github.com>
2019-08-02 13:35:48 +0530

    Create arrray.py

---
+++ arrray.py
@@ -0,0 +1,11 @@
+import math
+vizh,pop=map(int,input().split())
+gig=[]
+bob=list(map(int,input().split()))
+for k in range(0,pop):
+    vj,dig=map(int,input().split())
+    gig.append([vj,dig])
+for k in g:
+    shiv=k[0]-1
+    vani=k[1]-1
+    print(math.gcd(bob[shiv],bob[vani]))
