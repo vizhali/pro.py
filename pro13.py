536d84804b6872375a5080e990b91289283958d5
vizhali <52244020+vizhali@users.noreply.github.com>
2019-08-02 14:07:09 +0530

    Update pro13.py

--- pro13.py
+++ pro13.py
@@ -1,11 +1,11 @@
-vizh,pop=map(int,input().split())
+pop,vizh=map(int,input().split())
 lst=list(map(int,input().split()))
 l=[]
-for i in range(0,pop):
+for i in range(0,vizh):
      x,y=map(int,input().split())
      l.append([x,y])
-for i in range(pop):
+for i in range(vizh):
      lwr=l[i][0]
      upr=l[i][1]
-     shiv=sum(lst[lwr-1:upr])
-     print(shiv)
+     shivan=sum(lst[lwr-1:upr])
+     print(shivan)
