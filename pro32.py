b3bf2e65428a6f5d4a9845c7540e1ffc56160a5a
vizhali <52244020+vizhali@users.noreply.github.com>
2019-08-05 15:27:23 +0530

    Create pro32.py

---
+++ pro32.py
@@ -0,0 +1,11 @@
+vizh,pop=map(int,input().split())
+lst=[]
+for _ in range(vizh):
+	lt.append(sorted(list(map(int,input().split()))))
+for i in range(vizh-1):
+	for j in range(pop):
+		for k in range(vizh-i):
+			if(lst[i][j]>lst[i+k][j]):
+				lst[i][j],lst[i+k][j]=lst[i+k][j],lst[i][j]
+for i in lst:
+	print(*i,sep=' ')       
