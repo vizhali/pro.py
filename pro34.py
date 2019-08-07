0176121b919bd708bf782009b934b0614bed7e9b
vizhali <52244020+vizhali@users.noreply.github.com>
2019-08-07 14:35:47 +0530

    Update pro34.py

--- pro34.py
+++ pro34.py
@@ -6,7 +6,7 @@ for i in range(vizh):
 for i in range(vizh):
       for j in range(pop-1):
             if(Liee1[i][j]!='R' and Liee1[i][j+1]!='R'):
-                  mnt1+=3
+                  shiv1+=3
             elif(Liee1[i][j]!='G' and Liee1[i][j+1]!='G'):
                   shiv1+=5
 print(shiv1)
