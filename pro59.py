70ea2dc8aaa32deab39de9cf9fe91692a96b913b
vizhali <52244020+vizhali@users.noreply.github.com>
2019-08-09 14:53:36 +0530

    Create pro59.py

---
+++ pro59.py
@@ -0,0 +1,10 @@
+bhte,sbte=map(str,input().split("|"))
+ccte=input()
+if  len(bhte)>len(sbte):
+    if len(bhte)==len(sbte)+len(ccte):
+        print(bhte+"|"+sbte+ccte)
+elif len(bhte)<len(sbte):
+     if len(sbte)==len(bhte)+len(ccte):
+        print(bhet+ccte+"|"+sbte)
+elif len(bhte)==len(sbte) and len(ccte)>1 or (len(sbte) or len(bhte)):
+    print("impossible")
