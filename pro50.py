fa701ae24cdba7f63cc6c76aafdf717cb545b367
vizhali <52244020+vizhali@users.noreply.github.com>
2019-08-09 13:58:32 +0530

    Update pro50.py

--- pro50.py
+++ pro50.py
@@ -1,7 +1,24 @@
-vizh,pop,ccte=map(int,input().split())
-if vizh==224:
-	print("YES")
-elif(vizh%(pop+ccte)==0):
-	print("YES")
+xtee,ytee=map(int,input().split())
+ztee=[]
+
+for i in range(ytee):
+  ztee.append(list(map(int,input().split())))
+vizh=10000000
+pop=0
+
+for i in range(ytee):
+  if ztee[i][0]==1:
+    sont=ztee[i][1]
+    pict=ztee[i][2]
+    for j in range(i+1,ytee):
+      if ztee[j][0]==sont:
+        sont=ztee[j][1]
+        pict=pict+ztee[j][2]
+    if pict<vizh and sont==xtee:
+      vizh=pict
+      pop=pop+1
+
+if pop==0:
+  print(-1)
 else:
-	print("NO")
+  print(vizh)
