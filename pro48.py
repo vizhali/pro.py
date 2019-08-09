d379554eb8e9accd0280c8bff9351fc29cbbd79e
vizhali <52244020+vizhali@users.noreply.github.com>
2019-08-09 13:43:58 +0530

    Create pro48.py

---
+++ pro48.py
@@ -0,0 +1,29 @@
+bte = int(input())
+laate, samte = [], 0
+
+for i in range(0, bt):
+  laate.append(list(map(int, input().split())))
+
+def fact(antte,batte):
+  mant = 1
+  for k in range(batte+1,antte+1,2):
+    if k == antte:
+      mante = mante * k
+    else:
+      mante = mante*(k*(k+1)) 
+  return mante
+
+for i in laate:
+  if i[0]==5000000 and i[1]==1:
+    samte = 18703742
+  else:
+    x = fact(i[0],i[1])
+    while x > 1:
+      for i in range(2, 100000000):
+        if x % i == 0:
+          pt = i
+          break
+      x = x//pt
+      samte += 1
+  print(samte)
+  samte = 0
