b9ed0584ef1b08150c8204d5756ac6eebd7cd954
vizhali <52244020+vizhali@users.noreply.github.com>
2019-08-09 14:12:08 +0530

    Update pro52.py

--- pro52.py
+++ pro52.py
@@ -1,15 +1,15 @@
 import sys, string, math
 
-nunte = 4
+nunter = 4
 Laate = []
-for i in range(0,nunte) :
+for i in range(0,nunter) :
     Laate.append([])
 Lte2 = []
-for i in range(0,nunte) :
+for i in range(0,nunter) :
    Laate[i] = [ int(x) for x in input().split()]
 xte1 = Laate[0][0]
 yte1 = Laate[0][1]
-for i in range(1,nunte) :
+for i in range(1,nunter) :
     if Laate[i][0] != xte1 and Laate[i][1] != yte1 :
         xte3 = Laate[i][0]
         yte3 = Laate[i][1]
@@ -17,13 +17,13 @@ for i in range(1,nunte) :
         break
 
 Lte3 = [0,i2]
-for i in range(1,nunte) :
+for i in range(1,nunter) :
     if i != i2  :
         xte2 = Laate[i][0]
         yte2 = Laate[i][1]
         Lte3.append(i)
         break
-for i in range(1,nunte) :
+for i in range(1,nunter) :
     if i not in Lte3  :
         xte4 = Laate[i][0]
         yte4 = Laate[i][1]
