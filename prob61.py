b4ca5c8710a0cc211687f104d232d745581a4dd6
vizhali <52244020+vizhali@users.noreply.github.com>
2019-08-16 13:39:33 +0530

    Update prob61.py

--- prob61.py
+++ prob61.py
@@ -1,20 +1,20 @@
-def charming(loil):
-        bobl=1        
-        for p in range(0,len(loil)-1):
-                if loil[p]!=loil[p+1]:
-                        bobl=bobl+1
+def charming(loill):
+        bobll=1        
+        for p in range(0,len(loill)-1):
+                if loill[p]!=loill[p+1]:
+                        bobll=bobll+1
                 else:
                     break
-        return bobl
-chal=int(input())
-chol=list(map(int,input().split()))
-for p in range(0,len(chol)):
-        if chol[p]>0:
-                chol[p]=1
+        return bobll
+chall=int(input())
+choll=list(map(int,input().split()))
+for p in range(0,len(choll)):
+        if choll[p]>0:
+                choll[p]=1
         else:
-             chol[p]=0
-chel=""
-for p in range(0,len(chol)):
-        babal=chol[p::]
-        chel=chel+str(charming(babal))+" "
-print(chel.strip())
+             choll[p]=0
+chell=""
+for p in range(0,len(choll)):
+        baball=choll[p::]
+        chell=chell+str(charming(baball))+" "
+print(chell.strip())
